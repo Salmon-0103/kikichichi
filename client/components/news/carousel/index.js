ae8600b433b8787e468076928619e00da460e967
Salmon-0103 <absolute.still@gmail.com>
2025-02-08 15:43:44 +0800

    首頁新增輪播區

---
+++ client/components/news/carousel/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styles from './index.module.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+export default function Carousel() {
+	return (
+		<div id="carouselSlideOnly" className="carousel carousel-fade" data-bs-ride="carousel">
+			<div class="carousel-inner m-0 p-0">
+				<div
+					className={`${styles['carouselSize']} carousel-item active `}
+					data-bs-interval="3000"
+				>
+					<img src="/photos/articles/dessert_life.jpg" />
+				</div>
+				<div className={`${styles['carouselSize']} carousel-item`} data-bs-interval="3000">
+					<img src="/photos/articles/eatDessertTime.jpg" />
+				</div>
+				<div className={`${styles['carouselSize']} carousel-item`} data-bs-interval="3000">
+					<img src="/photos/articles/BerryUP.jpg" />
+				</div>
+			</div>
+
+			{/* 手動控制按鈕 */}
+			<button
+				className={`${styles['carouselBtnLeft']} carousel-control-prev`}
+				type="button"
+				data-bs-target="#carouselSlideOnly"
+				data-bs-slide="prev"
+			>
+				<span
+					className={`${styles['carouselIcon']} carousel-control-prev-icon`}
+					aria-hidden="true"
+				></span>
+				<span className="visually-hidden">Previous</span>
+			</button>
+			<button
+				className={`${styles['carouselBtnRight']} carousel-control-next`}
+				type="button"
+				data-bs-target="#carouselSlideOnly"
+				data-bs-slide="next"
+			>
+				<span
+					className={`${styles['carouselIcon']} carousel-control-next-icon`}
+					aria-hidden="true"
+				></span>
+				<span className="visually-hidden">Next</span>
+			</button>
+		</div>
+	);
+}
