24e5f382fa6d1b1d6efce5df0bb9b524292c5a81
Salmon-0103 <absolute.still@gmail.com>
2025-02-11 18:41:12 +0800

    更新照片

--- client/components/news/carousel/index.js
+++ client/components/news/carousel/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './index.module.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export default function Carousel() {
 	return (
