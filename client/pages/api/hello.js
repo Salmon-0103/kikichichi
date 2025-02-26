0a33732d758113345b904e7dd782d0157a4c3cd6
Salmon-0103 <absolute.still@gmail.com>
2025-02-26 21:26:02 +0800

    Revert "測試"
    This reverts commit f8e18a34491dff485f525810e93f17096736665b.

--- client/pages/api/hello.js
+++ client/pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-	res.status(200).json({ message: 'API 運作正常' });
+  res.status(200).json({ name: 'John Doe' })
 }
